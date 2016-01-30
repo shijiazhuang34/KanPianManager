5c6b68994a0b72d5b3fe6608c9981b4637f8d2d6
Meteor <justlikemaki@qq.com>
2016-01-30 17:46:58 +0800

    更新初始数据

--- sql/data/split/public.javsrc.sql.bat
+++
@@ -1,16 +0,0 @@
-@echo off 
-echo -----------------------------------------------------
-echo -               欢迎使用软媒分割合并器              -
-echo -                                                   -
-echo -                                                   -
-echo ------------软媒官网：www.ruanmei.com----------------
-echo ------------软媒魔方官网：mofang.ruanmei.com---------
-echo -----------------------------------------------------
-echo 本批处理文件共分成 50 个子文件，共 814.11 M
-echo 请确认: 1、子文件的个数是否相符并放在当前目录下
-echo         2、可用剩余空间是否足够
-pause
-echo 开始合并文件, 合并过程中请勿关闭，合并时间长短与文件的大小有关
-echo 合并结束后会自动关闭当前窗口
-echo 请稍后... 
-copy /b public.javsrc.sql.rm1+public.javsrc.sql.rm2+public.javsrc.sql.rm3+public.javsrc.sql.rm4+public.javsrc.sql.rm5+public.javsrc.sql.rm6+public.javsrc.sql.rm7+public.javsrc.sql.rm8+public.javsrc.sql.rm9+public.javsrc.sql.rm10+public.javsrc.sql.rm11+public.javsrc.sql.rm12+public.javsrc.sql.rm13+public.javsrc.sql.rm14+public.javsrc.sql.rm15+public.javsrc.sql.rm16+public.javsrc.sql.rm17+public.javsrc.sql.rm18+public.javsrc.sql.rm19+public.javsrc.sql.rm20+public.javsrc.sql.rm21+public.javsrc.sql.rm22+public.javsrc.sql.rm23+public.javsrc.sql.rm24+public.javsrc.sql.rm25+public.javsrc.sql.rm26+public.javsrc.sql.rm27+public.javsrc.sql.rm28+public.javsrc.sql.rm29+public.javsrc.sql.rm30+public.javsrc.sql.rm31+public.javsrc.sql.rm32+public.javsrc.sql.rm33+public.javsrc.sql.rm34+public.javsrc.sql.rm35+public.javsrc.sql.rm36+public.javsrc.sql.rm37+public.javsrc.sql.rm38+public.javsrc.sql.rm39+public.javsrc.sql.rm40+public.javsrc.sql.rm41+public.javsrc.sql.rm42+public.javsrc.sql.rm43+public.javsrc.sql.rm44+public.javsrc.sql.rm45+public.javsrc.sql.rm46+public.javsrc.sql.rm47+public.javsrc.sql.rm48+public.javsrc.sql.rm49+public.javsrc.sql.rm50 public.javsrc.sql
